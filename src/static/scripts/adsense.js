16075765793dddf42f851be4a285423cb0b969f0
Leandro Oriente <leandroriente@gmail.com>
2014-01-09 02:28:34 -0200

    Sidebar ad no post

--- src/static/scripts/adsense.js
+++ src/static/scripts/adsense.js
@@ -5,13 +5,14 @@ $(document).ready(function(){
   var _browserSize = $(window).width(),
       ads = {},
       $header = $('#header-ad'),
+      $sidePost = $('#side-post-ad'),
       $middlePost = $('#middle-posts-ad'),
       $footerPost = $('#footer-post-ad');
 
   ads.fillDesktopAds = function(){
     ads.renderElem('ca-pub-1601752090500951', '8329748429', $middlePost, 'ad-970');
     ads.renderElem('ca-pub-1601752090500951', '6713414425', $footerPost, 'ad-970');
-    ads.renderElem('ca-pub-1601752090500951', '7278490821', $header, 'ad-header');
+    ads.renderElem('ca-pub-1601752090500951', '7278490821', $header);
   };
 
   ads.fillTabletAds = function(){
@@ -26,13 +27,43 @@ $(document).ready(function(){
 
   ads.renderElem = function(adClient, adSlot, elem, elemClass) {
     if (elem.length) {
-      elem.data('ad-client', adClient).data('ad-slot' , adSlot)
-        .addClass(elemClass);
+      elem.data('ad-client', adClient).data('ad-slot' , adSlot);
+      if (elemClass){
+        elem.addClass(elemClass);
+      }
       adsbygoogle.push({});
     }
   };
 
-  if (_browserSize > 900) {
+  ads.sidePostFixedAd = function() {
+    ads.renderElem('ca-pub-1601752090500951', '5236681221', $sidePost);
+
+    var $postItem = $('.post-item'),
+        _offsetAd = $sidePost.offset(),
+        _offsetPost = $postItem.offset(),
+        _adMgt = 20;
+
+    $(window).scroll(function(){
+      if ($(window).scrollTop() < _offsetPost.top - _adMgt) {
+        $sidePost.addClass('ad-side-post').removeClass('ad-fixed-side-post ad-abs-bottom-side-post');
+      } else if ($(window).scrollTop() < (_offsetPost.top + $postItem.height() - ($sidePost.height() + _adMgt))) {
+        console.log('Passei o post');
+
+        $sidePost.addClass('ad-fixed-side-post').removeClass('ad-side-post ad-abs-bottom-side-post');
+        $sidePost.css({
+          'left' : _offsetAd.left,
+          'top' : 20
+        });
+      } else {
+        $sidePost.addClass('ad-abs-bottom-side-post').removeClass('ad-side-post ad-fixed-side-post');
+      }
+    });
+  };
+
+  if (_browserSize > 1270) { 
+    ads.sidePostFixedAd();
+    ads.fillDesktopAds();
+  } else if (_browserSize > 900) {
     ads.fillDesktopAds();
   } else if (_browserSize > 480) {
     ads.fillTabletAds();
