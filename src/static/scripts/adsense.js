11378113f2d31de59c7fe606aec8f362f0b98e0a
Leandro Oriente <leandroriente@gmail.com>
2014-01-21 02:49:11 -0200

    Fix adsemse.js˜

--- src/static/scripts/adsense.js
+++ src/static/scripts/adsense.js
@@ -4,7 +4,6 @@ $(document).ready(function(){
 
   var _browserSize = $(window).width(),
       ads = {},
-      $header = $('#header-ad'),
       $sidePost = $('#side-post-ad'),
       $middlePost = $('#middle-posts-ad'),
       $footerPost = $('#footer-post-ad');
@@ -12,17 +11,6 @@ $(document).ready(function(){
   ads.fillDesktopAds = function(){
     ads.renderElem('ca-pub-1601752090500951', '8329748429', $middlePost, 'ad-970');
     ads.renderElem('ca-pub-1601752090500951', '6713414425', $footerPost, 'ad-970');
-    ads.renderElem('ca-pub-1601752090500951', '7278490821', $header);
-  };
-
-  ads.fillTabletAds = function(){
-    ads.renderElem('ca-pub-1601752090500951', '2283214825', $middlePost, 'ad-728');
-    ads.renderElem('ca-pub-1601752090500951', '8190147626', $footerPost, 'ad-728');
-  };
-
-  ads.fillMobileAds = function(){
-    ads.renderElem('ca-pub-1601752090500951', '3759948022', $middlePost, 'ad-320');
-    ads.renderElem('ca-pub-1601752090500951', '9666880822', $footerPost, 'ad-320');
   };
 
   ads.renderElem = function(adClient, adSlot, elem, elemClass) {
@@ -67,9 +55,5 @@ $(document).ready(function(){
     ads.fillDesktopAds();
   } else if (_browserSize > 900) {
     ads.fillDesktopAds();
-  } else if (_browserSize > 480) {
-    ads.fillTabletAds();
-  } else {
-    ads.fillMobileAds();
   }
 });
