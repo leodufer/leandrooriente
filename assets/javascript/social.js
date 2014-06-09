8040b7803ca277c578a9b6ac3be6504a944fe4ac
leandrooriente <leandroriente@gmail.com>
2014-06-09 12:36:43 -0300

    Blink share

--- assets/javascript/social.js
+++ assets/javascript/social.js
@@ -2,33 +2,45 @@ var loadSocial = function(){
   // FB
   loadFacebook(document, 'script', 'facebook-jssdk');
 
-  loadTwitter(document, 'script', 'twitter-wjs');
+  loadTwitter();
+};
+
+var smileBlink = function() {
+  $('.ico-smiley')
+    .removeClass('ico-smiley')
+    .addClass('ico-wink');
 };
 
 var loadFacebook = function(d, s, id) {
-  var js, 
-      fjs = d.getElementsByTagName(s)[0];
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '479584285400462',
+      xfbml      : true,
+      version    : 'v2.0'
+    });
+    FB.Event.subscribe('edge.create', smileBlink);
+  };
 
-  if (d.getElementById(id)) return;
-
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=479584285400462";
-  fjs.parentNode.insertBefore(js, fjs);
-
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
 };
 
-var loadTwitter = function(d,s,id){
-  var js,
-      fjs = d.getElementsByTagName(s)[0],
-      p = /^http:/.test(d.location)?'http':'https';
+var loadTwitter = function(){
+  window.twttr = (function (d,s,id) {
+    var t, js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return; js=d.createElement(s); js.id=id;
+    js.src="https://platform.twitter.com/widgets.js"; fjs.parentNode.insertBefore(js, fjs);
+    return window.twttr || (t = { _e: [], ready: function(f){ t._e.push(f) } });
+  }(document, "script", "twitter-wjs"));
 
-  if (!d.getElementById(id)){
-    js = d.createElement(s);
-    js.id = id;
-    js.src = p+'://platform.twitter.com/widgets.js';
-    fjs.parentNode.insertBefore(js,fjs);
-  }
-
-};
+  twttr.ready(function (twttr) {
+    twttr.events.bind('click', smileBlink);
+  });
+}
 
 window.onload = loadSocial;
